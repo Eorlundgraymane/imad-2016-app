43c29c4e319a927ec69319f883c547a803e92451
eorlundgraymane <rkmenon235@gmail.com>
2016-09-23 20:34:46 +0530

    Code Forge Emblem

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/Coder.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Coder.png'));
 });
-
+app.get('/ui/skyforge.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'skyforge.png'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
