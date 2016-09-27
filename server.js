74cd9d6f0dfc6828b1f2713f7fdbf9bc02217580
eorlundgraymane <rkmenon235@gmail.com>
2016-09-27 19:39:27 +0530

    Update 27/10 : 7:39PM

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
@@ -25,6 +22,9 @@ app.get('/ui/skyforge.png', function (req, res) {
 app.get('/ui/Realm.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Realm.html'));
 });
+app.get('/ui/main.js',function (req,res){
+  res.sendFile(path.join(__dirname,'ui','main.js'));
+})
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
