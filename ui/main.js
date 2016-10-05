578109aa644566a8b09f7fbec400691ff43c6060
eorlundgraymane <rkmenon235@gmail.com>
2016-10-05 20:50:45 +0530

    Website Revamp

--- ui/main.js
+++ ui/main.js
@@ -9,4 +9,16 @@ $(document).ready(function() {
     var newvalueY = height * pageY * -10 + 50;
     $('#container').css("background-position", newvalueX + "px     " + newvalueY + "px");
   });
+  $("#container").mousemove(function(e) {
+    var pageX = e.pageX - ($(window).width() / 2);
+    var pageY = e.pageY - ($(window).height() / 2);
+    var newvalueX = width * pageX * -1 ;
+    var newvalueY = height * pageY * -1;
+    $('.navi').css("margin-left",newvalueX + "px");
+    $('.navi').css("margin-top", newvalueY + "px");
+    $('.propic').css("margin-left",newvalueX + "px");
+    $('.propic').css("margin-top", newvalueY + "px");
+    $('#header').css("margin-left",newvalueX + "px");
+    $('#header').css("margin-top", newvalueY + "px");
+  });
 });
