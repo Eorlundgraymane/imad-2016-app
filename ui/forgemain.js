0a98b7ea898b8c57b9b3eaa5454cc1cd317a588b
eorundgraymane <rkmenon235@gmail.com>
2016-10-25 21:19:15 +0530

    Update 25/Oct 9:20PM

--- ui/forgemain.js
+++ ui/forgemain.js
@@ -1,12 +1,10 @@
-$(document).ready(function() {
-  var movementStrength = 25;
-  var height = movementStrength / $(window).height();
-  var width = movementStrength / $(window).width();
-    $("#container").mousemove(function(e) {
-    var pageX = e.pageX - ($(window).width() / 2);
-    var pageY = e.pageY - ($(window).height() / 2);
-    var newvalueX = width * pageX * -3 + 200;
-    var newvalueY = height * pageY * -3 + 50;
-    $('#container').css("background-position", newvalueX + "px     " + newvalueY + "px");
-  });
-});
+function view()
+{
+	alert("View Function called");
+}
+function post()
+{
+	alert("Post function ready");
+	var timeline = document.getElementById('blog');
+	timeline.innerHTML = '<fieldset><legend>Post Pad</legend><font color = "white"><textarea name = "textpost" style = "border-width: 0px; color: white; background-color: black" cols = "50" rows = "30" placeholder = "Post your thoughts"></textarea></font><br><input type = "submit" style = width: 150px;" value = "Post"/></fieldset>';
+}
