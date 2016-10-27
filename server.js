7e4ece4dbc2fc643f3cb087bb3b33126698dfb28
eorundgraymane <rkmenon235@gmail.com>
2016-10-27 19:32:13 +0530

    DB Phase 1 Level 2.9.5

--- server.js
+++ server.js
@@ -15,6 +15,27 @@ app.use(morgan('combined'));
 
 var pool = new Pool(config);//create connection pool object
 
+app.get('/', function (req, res) {//Root site directory. the first page that gets opened
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/style.css', function (req, res) {//the style page for the home page
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/madi.png', function (req, res) {//image file
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+app.get('/Coder.png', function (req, res) {//image file
+  res.sendFile(path.join(__dirname, 'ui', 'Coder.png'));
+});
+app.get('/skyforge.png', function (req, res) {//image file
+  res.sendFile(path.join(__dirname, 'ui', 'skyforge.png'));
+});
+app.get('/main.js', function (req, res) {//the main javascript file
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+app.get('/codeforge',function (req,res){//the first of the three blog pages
+    res.sendFile(path.join(__dirname,'ui', 'codeforge.html'));
+});
 app.get('/userlist',function(req,res){//in case the db is called
 	pool.query('SELECT username FROM "Accounts"' , function(err , result){//what query to be passed
 		if(err){
@@ -24,27 +45,6 @@ app.get('/userlist',function(req,res){//in case the db is called
 			res.send(JSON.stringify(result.rows));//else return the query result as a JSON string
 		}
 	});
-});
-app.get('/', function (req, res) {//Root site directory. the first page that gets opened
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-app.get('/ui/style.css', function (req, res) {//the style page for the home page
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-app.get('/ui/madi.png', function (req, res) {//image file
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-app.get('/ui/Coder.png', function (req, res) {//image file
-  res.sendFile(path.join(__dirname, 'ui', 'Coder.png'));
-});
-app.get('/ui/skyforge.png', function (req, res) {//image file
-  res.sendFile(path.join(__dirname, 'ui', 'skyforge.png'));
-});
-app.get('/ui/main.js', function (req, res) {//the main javascript file
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-app.get('/codeforge',function (req,res){//the first of the three blog pages
-    res.sendFile(path.join(__dirname,'ui', 'codeforge.html'));
 });
 app.get('/ui/forgestyle.css',function (req,res){//style of the codeforge page
     res.sendFile(path.join(__dirname,'ui', 'forgestyle.css'));
