f9df3243432ef7882f7d4cca6fabb2e438365de3
Eorlund Graymane <rkmenon235@gmail.com>
2016-11-07 16:18:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,16 +28,16 @@ app.get('/hash/:input',function (req,res){
 }); 
 
 app.post('/login',function(req,res){	
-	var username = req.body.username;
+	var email = req.body.email;
 	var password = req.body.password;	
-	pool.query('SELECT * FROM users WHERE username like $1',[username],function(err,result){
+	pool.query('SELECT * FROM codeusers WHERE email like $1',[email],function(err,result){
 			if(err){
 			res.send(500).send(err.toString());
 		}
 		else
 			{
 					if(result.rows.length ===0){
-						res.send(403).send('username/password is invalid');
+						res.send(403).send('email/password is invalid');
 					}
 					else{
 						var dbString = result.rows[0].passwordhash;
