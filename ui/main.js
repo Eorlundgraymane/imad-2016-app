98f00337d1c5cf530d5d5bf02c5aab5bf87b031b
Eorlund Graymane <rkmenon235@gmail.com>
2016-11-07 18:39:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,41 +1,4 @@
-function signupready()
-{
-    alert("Sign Up Script Ready");
-    var regy = document.getElementById('signupbutton');
-    regy.onclick = function(){
-    	var email = document.getElementById('email').value;
-    	var password = document.getElementById('password').value;
-    	var fname = document.getElementById('fname').value;
-    	var lname = document.getElementById('lname').value;
-    	var dob = document.getElementById('date').value;
-    
-    	var request = new XMLHttpRequest();
-    	request.onreadystatechange = function(){
-    		if(request.readyState === XMLHttpRequest.DONE){
-    			if(request.status === 502)
-    				{
-    					console.log("user created");
-    					alert("Creates Account successfully");		
-    				}
-    			else if(request.status === 403){
-    				alert('Something is 403');
-    			}
-    			else if(request.status === 500){
-    				alert('Something is wrong on the server 500');
-    			}
-    		}
-    	};
-    console.log(dob);
-    console.log(email);
-    request.open('POST','http://eorlundgraymane.imad.hasura-app.io/create-user',true);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({fname:fname,lname:lname,email:email,password:password,dob:dob}));
-    };
-}
-
-function loginready()
-{
-    alert("Log In Script Ready");
+     alert("Log In Script Ready");
     var submit = document.getElementById('loginbutton');
     submit.onclick = function(){
     var email = document.getElementById('emailin').value;
@@ -62,5 +25,3 @@ function loginready()
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({email:email,password:password}));
     };
-}
-document.ready(function(){signupready();loginready();});
