a4569f32605a7e03118ae41791842002f0ae8f08
Eorlund Graymane <rkmenon235@gmail.com>
2016-11-07 19:01:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+       alert("Log In Script Ready");
     var submit = document.getElementById('loginbutton');
     submit.onclick = function(){
     var email = document.getElementById('emailin').value;
@@ -19,7 +20,7 @@ window.onload = function(){
     			}
     		}
     	};
-    alert("Log In Script Ready");
+
     console.log(email);
     console.log(password);
     request.open('POST','http://eorlundgraymane.imad.hasura-app.io/login',true);
