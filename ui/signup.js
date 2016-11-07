6ce875e06a96256717bdbd23a7bc1e610ed821b1
Eorlund Graymane <rkmenon235@gmail.com>
2016-11-07 18:39:59 +0530

    [imad-console] Updates ui/signup.js

--- ui/signup.js
+++ ui/signup.js
@@ -1,31 +1,31 @@
-alert("Sign Up Script Ready");
-var regy = document.getElementById('signupbutton');
-regy.onclick = function(){
+    alert("Sign Up Script Ready");
+    var regy = document.getElementById('signupbutton');
+    regy.onclick = function(){
     	var email = document.getElementById('email').value;
-	var password = document.getElementById('password').value;
-	var fname = document.getElementById('fname').value;
-	var lname = document.getElementById('lname').value;
-	var dob = document.getElementById('date').value;
-
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function(){
-		if(request.readyState === XMLHttpRequest.DONE){
-			if(request.status === 502)
-				{
-					console.log("user created");
-					alert("Creates Account successfully");		
-				}
-			else if(request.status === 403){
-				alert('Something is 403');
-			}
-			else if(request.status === 500){
-				alert('Something is wrong on the server 500');
-			}
-		}
-	};
-		console.log(dob);
-console.log(email);
-request.open('POST','http://eorlundgraymane.imad.hasura-app.io/create-user',true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({fname:fname,lname:lname,email:email,password:password,dob:dob}));
-};
+    	var password = document.getElementById('password').value;
+    	var fname = document.getElementById('fname').value;
+    	var lname = document.getElementById('lname').value;
+    	var dob = document.getElementById('date').value;
+    
+    	var request = new XMLHttpRequest();
+    	request.onreadystatechange = function(){
+    		if(request.readyState === XMLHttpRequest.DONE){
+    			if(request.status === 502)
+    				{
+    					console.log("user created");
+    					alert("Creates Account successfully");		
+    				}
+    			else if(request.status === 403){
+    				alert('Something is 403');
+    			}
+    			else if(request.status === 500){
+    				alert('Something is wrong on the server 500');
+    			}
+    		}
+    	};
+    console.log(dob);
+    console.log(email);
+    request.open('POST','http://eorlundgraymane.imad.hasura-app.io/create-user',true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({fname:fname,lname:lname,email:email,password:password,dob:dob}));
+    };
