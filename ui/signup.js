f724faf5398137240ea07487a292ffdf82da0c81
eorundgraymane <rkmenon235@gmail.com>
2016-11-07 12:22:56 +0530

    Register and Login Utiliyu Complete

--- ui/signup.js
+++ ui/signup.js
@@ -1,18 +1,19 @@
-window.onload = function(){var submit = document.getElementById('loginbutton');}
-submit.onclick = function(){
+var regy = document.getElementById('signupbutton');
+alert(regy);
+regy.onclick = function(){
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function(){
 		if(request.readyState === XMLHttpRequest.DONE){
 			if(request.status === 200)
 				{
-					console.log("user logged in");
-					alert("Logged in successfully");		
+					console.log("user created");
+					alert("Creates Account successfully");		
 				}
 			else if(request.status === 403){
-				alert('Username/Password is invalid');
+				alert('Something is 404');
 			}
 			else if(request.status === 500){
-				alert('Something is wrong on the server');
+				alert('Something is wrong on the server 500');
 			}
 		}
 	};
@@ -20,7 +21,7 @@ var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 console.log(username);
 console.log(password);
-request.open('POST','http://localhost:8080/login',true);
+request.open('POST','http://localhost:8080/create-user',true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username:username,password:password}));
 };
