a022536f021efc1701ed033afb00c844027ca833
Eorlund Graymane <rkmenon235@gmail.com>
2016-11-07 16:18:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,11 +16,11 @@ submit.onclick = function(){
 			}
 		}
 	};
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
+var email = document.getElementById('emailin').value;
+var password = document.getElementById('passin').value;
 console.log(username);
 console.log(password);
 request.open('POST','http://eorlundgraymane.imad.hasura-app.io/login',true);
 request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({username:username,password:password}));
+request.send(JSON.stringify({email:email,password:password}));
 };
