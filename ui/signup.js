97656d4fe043edaeb405c58034963f12a22d8306
Eorlund Graymane <rkmenon235@gmail.com>
2016-11-07 15:56:21 +0530

    [imad-console] Updates ui/signup.js

--- ui/signup.js
+++ ui/signup.js
@@ -2,8 +2,16 @@ alert("Ready");
 var regy = document.getElementById('signupbutton');
 alert(regy.value);
 regy.onclick = function(){
-    alert("Tell Roshan that you saw this "+document.getElementById('email').value);
-    alert("Tell Roshan that you saw this "+document.getElementById('password').value);
+    	var email = document.getElementById('email').value;
+	var password = document.getElementById('password').value;
+	var fname = document.getElementById('fname').value;
+	var lname = document.getElementById('lname').value;
+	var dob = document.getElementById('date').value;
+	console.log(dob);
+console.log(email);
+request.open('POST','http://eorlundgraymane.imad.hasura-app.io/create-user',true);
+request.setRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({fname:fname,lname:lname,email:email,password:password,dob:dob}));
 	var request = new XMLHttpRequest();
 	request.onreadystatechange = function(){
 		if(request.readyState === XMLHttpRequest.DONE){
@@ -20,14 +28,4 @@ regy.onclick = function(){
 			}
 		}
 	};
-	var email = document.getElementById('email').value;
-	var password = document.getElementById('password').value;
-	var fname = document.getElementById('fname').value;
-	var lname = document.getElementById('lname').value;
-	var dob = document.getElementById('date').value;
-	console.log(dob);
-console.log(email);
-request.open('POST','http://eorlundgraymane.imad.hasura-app.io/create-user',true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({fname:fname,lname:lname,email:email,password:password,dob:dob}));
 };
