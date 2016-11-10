01de1dc3130101b17596b2b64339d13822c8688f
Eorlund Graymane <rkmenon235@gmail.com>
2016-11-10 09:37:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;//Postgres connetion pool
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 var config = 
 		{
 			user: 'eorlundgraymane',//credentials used to login to db in this case imad db
@@ -15,6 +16,10 @@ var config =
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret: "someRandomSecretKey",
+    cookie: {maxAge: 1000*60*60*24*30}
+}));
 
 var pool = new Pool(config);//create connection pool object
 
@@ -44,7 +49,9 @@ app.post('/login',function(req,res){
 						var salt = dbString.split('$')[2];
 						var hashedPassword = hash(password,salt);
 						if(hashedPassword === dbString){
-							res.send('credentials are correct');
+						    req.session.auth = {useId: result.rows[0].id};
+							res.send(200).send('credentials are correct');
+							
 						}
 						else
 							{
