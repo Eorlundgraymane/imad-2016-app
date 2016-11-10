3b1d71824ac36693b12a13ada60ca53e04365a37
Eorlund Graymane <rkmenon235@gmail.com>
2016-11-10 09:42:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/login',function(req,res){
 			}
 	});
 });
-app.get('/chech-login',function(req,res){
+app.get('/check-login',function(req,res){
    if(req.session && req.session.auth && req.session.auth.userId)
    {
        res.send("User logged in "+req.session.auth.userId.toString());
